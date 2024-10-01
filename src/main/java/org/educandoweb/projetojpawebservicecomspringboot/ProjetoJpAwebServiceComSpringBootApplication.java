3334f5c90cd62e8d4e67bedb5b796d75b36d68fa
Sandriely F M Toledo GTADSI33A <109091362+Sandriely1999@users.noreply.github.com>
2024-09-30 22:09:50 -0300

    Project created

---
+++ src/main/java/org/educandoweb/projetojpawebservicecomspringboot/ProjetoJpAwebServiceComSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.educandoweb.projetojpawebservicecomspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoJpAwebServiceComSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoJpAwebServiceComSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/educandoweb/projetojpawebservicecomspringboot/ProjetoJpAwebServiceComSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.educandoweb.projetojpawebservicecomspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoJpAwebServiceComSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
