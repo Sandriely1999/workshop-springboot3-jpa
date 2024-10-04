a794715dc211b5a9af1ebc616d620c3779c8f9ae
Sandriely F M Toledo GTADSI33A <109091362+Sandriely1999@users.noreply.github.com>
2024-10-03 23:00:10 -0300

    h2 database, test profile, JPA

--- src/main/java/org/educandoweb/projetojpawebservicecomspringboot/entities/User.java
+++ src/main/java/org/educandoweb/projetojpawebservicecomspringboot/entities/User.java
@@ -1,10 +1,17 @@
 package org.educandoweb.projetojpawebservicecomspringboot.entities;
 
+
+import javax.persistence.*;
 import java.io.Serializable;
 
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
