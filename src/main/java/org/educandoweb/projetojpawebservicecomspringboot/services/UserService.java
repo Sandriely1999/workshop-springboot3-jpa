5fbd10da64bcd5076b8df8c50914b30998bcea2a
Sandriely F M Toledo GTADSI33A <109091362+Sandriely1999@users.noreply.github.com>
2025-01-16 20:54:21 -0300

    User insert

--- src/main/java/org/educandoweb/projetojpawebservicecomspringboot/services/UserService.java
+++ src/main/java/org/educandoweb/projetojpawebservicecomspringboot/services/UserService.java
@@ -28,4 +28,10 @@ public class UserService {
         Optional<User> obj = userRepository.findById(id);
         return obj.get();
     }
+
+    public User insert(User obj) {
+        return userRepository.save(obj);
+    }
+
+
 }
