a035509f95f62770e92463b19841fefd61b31311
Sandriely F M Toledo GTADSI33A <109091362+Sandriely1999@users.noreply.github.com>
2025-01-17 19:08:24 -0300

    Exception handling - findById

--- src/main/java/org/educandoweb/projetojpawebservicecomspringboot/services/UserService.java
+++ src/main/java/org/educandoweb/projetojpawebservicecomspringboot/services/UserService.java
@@ -2,6 +2,7 @@ package org.educandoweb.projetojpawebservicecomspringboot.services;
 
 import org.educandoweb.projetojpawebservicecomspringboot.entities.User;
 import org.educandoweb.projetojpawebservicecomspringboot.repositories.UserRepository;
+import org.educandoweb.projetojpawebservicecomspringboot.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -26,7 +27,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
